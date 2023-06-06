9795c649f739eb715cd0c5bd3c3d5d9a4677b30b
François DESCAVES <fdescaves@users.noreply.github.com>
2023-06-06 22:19:11 +0200

    Update item endpoint & dependencies

--- src/TooGoodToGoNotifier/Services/EmailService.cs
+++ src/TooGoodToGoNotifier/Services/EmailService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using MailKit.Net.Smtp;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MimeKit;
@@ -12,43 +13,46 @@ namespace TooGoodToGoNotifier.Services
     {
         private readonly ILogger<EmailService> _logger;
         private readonly EmailServiceOptions _emailNotifierOptions;
+        private readonly IHostEnvironment _hostEnvironment;
 
-        public EmailService(ILogger<EmailService> logger, IOptions<EmailServiceOptions> emailNotifierOptions)
+        public EmailService(ILogger<EmailService> logger, IOptions<EmailServiceOptions> emailNotifierOptions, IHostEnvironment hostEnvironment)
         {
             _logger = logger;
             _emailNotifierOptions = emailNotifierOptions.Value;
+            _hostEnvironment = hostEnvironment;
         }
 
         public async Task SendEmailAsync(string subject, string body, string[] recipients)
         {
-            _logger.LogInformation("Sending email to {recipients}", string.Join(", ", recipients));
-
-#if !DEBUG
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(string.Empty, _emailNotifierOptions.SmtpUserName));
-
-            foreach (string recipient in recipients)
+            if (_hostEnvironment.IsProduction())
             {
-                message.Bcc.Add(new MailboxAddress(string.Empty, recipient));
+                _logger.LogInformation("Sending email to {recipients}", string.Join(", ", recipients));
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(string.Empty, _emailNotifierOptions.SmtpUserName));
+
+                foreach (string recipient in recipients)
+                {
+                    message.Bcc.Add(new MailboxAddress(string.Empty, recipient));
+                }
+
+                message.Subject = subject;
+
+                message.Body = new TextPart
+                {
+                    Text = body
+                };
+
+                using var client = new SmtpClient();
+
+                await client.ConnectAsync(_emailNotifierOptions.SmtpServer, _emailNotifierOptions.SmtpPort, _emailNotifierOptions.UseSsl);
+
+                await client.AuthenticateAsync(_emailNotifierOptions.SmtpUserName, _emailNotifierOptions.SmtpPassword);
+
+                await client.SendAsync(message);
+
+                await client.DisconnectAsync(true);
             }
-
-            message.Subject = subject;
-
-            message.Body = new TextPart
-            {
-                Text = body
-            };
-
-            using var client = new SmtpClient();
-
-            await client.ConnectAsync(_emailNotifierOptions.SmtpServer, _emailNotifierOptions.SmtpPort, _emailNotifierOptions.UseSsl);
-
-            await client.AuthenticateAsync(_emailNotifierOptions.SmtpUserName, _emailNotifierOptions.SmtpPassword);
-
-            await client.SendAsync(message);
-
-            await client.DisconnectAsync(true);
-#endif
         }
     }
 }
